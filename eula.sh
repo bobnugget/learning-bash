4bee55ee201de40f84fc6847e2124b8ecd635487
Bob Nugget <bob@nuggetlab.com>
2020-01-29 11:17:18 -0500

    Created EULA example to source another script

---
+++ eula.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "ATTENTION:"
+echo "This is a reminder that the company owns"
+echo "everything you do, think, say, imply, create,"
+echo "smell, and see. Unless that thing is offensive,"
+echo "in which case, your views do not represent"
+echo "the company in any way whatsoever."
+echo
+echo "Thank you. Carry on..."
+echo "---"
+echo
---
+++ sourcing_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source eula.sh
+
+echo "Ok, now that that's over -- LET'S BREAK STUFF!!!!"
